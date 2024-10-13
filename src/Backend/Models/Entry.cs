6d034b55acf1571e55feefe1275cff777f792e6e
brucengdev <brucengdev@gmail.com>
2024-10-13 13:34:22 +0700

    Added Database configureation and some model.

---
+++ src/Backend/Models/Entry.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models
+{
+    public class Entry
+    {
+        public float Amount { get; set; }
+        public DateTime Time { get; set; }
+
+        public string Description { get;set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ src/Backend/Models/ExpenesesContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public class ExpenesesContext: DbContext
+    {
+        public DbSet<Entry> Entries { get;set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
