6e83119157106a880abdde0b4ec890623bf7a9b9
brucengdev <brucengdev@gmail.com>
2024-12-26 01:03:41 +0700

    wip

--- src/webfrontend/src/Login.tsx
+++ src/webfrontend/src/Login.tsx
@@ -3,7 +3,7 @@ export function Login() {
       <h1>Login</h1>
       <label>
         Username
-        <input type="text" />
+        <input type="text" className="mandatory" />
       </label>
       <label>
         Password
--- src/webfrontend/src/__test__/Login.test.tsx
+++ src/webfrontend/src/__test__/Login.test.tsx
@@ -2,6 +2,7 @@ import { screen, render } from "@testing-library/react";
 import {describe, expect, it} from 'vitest'
 import '@testing-library/jest-dom'
 import { Login } from "../Login";
+import { userEvent } from "@testing-library/user-event";
 
 describe("Login", () => {
     it("has necessary ui components", () => {
@@ -11,5 +12,13 @@ describe("Login", () => {
         expect(screen.getByRole("textbox", { name: "Username"})).toBeInTheDocument()
         expect(screen.getByLabelText("Password")).toBeInTheDocument()
         expect(screen.getByRole("button", { name: "Login"})).toBeInTheDocument()
+    })
+
+    it("has compulsory username", () => {
+        render(<Login />)
+
+        userEvent.click(screen.getByRole("button", { name: "Login"}))
+
+        expect(screen.getByRole("textbox", { name: "Username" }).className).toContain("mandatory")
     })
 })
