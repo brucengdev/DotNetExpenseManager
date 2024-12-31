9551155a7cf67d2d5c3ae871c993679d90d6d6e2
brucengdev <brucengdev@gmail.com>
2025-01-01 03:27:55 +0700

    wip

--- src/Backend/Controllers/EntriesController.cs
+++ src/Backend/Controllers/EntriesController.cs
@@ -9,16 +9,18 @@ namespace Backend.Controllers;
 internal class EntriesController: ControllerBase
 {
     private readonly IEntryManager _entryManager;
+    private readonly IAccountManager _accountManager;
     internal EntriesController(IEntryManager em, IAccountManager am)
     {
         _entryManager = em;
+        _accountManager = am;
     }
 
     [HttpPost("[action]")]
     public ActionResult AddEntry(Entry inputEntry, string accessToken)
     {
         var resolvedEntry = new Entry(inputEntry);
-        resolvedEntry.UserId = 12;
+        resolvedEntry.UserId = _accountManager.GetUserId(accessToken);
         _entryManager.AddEntry(resolvedEntry);
         return Ok();
     }
