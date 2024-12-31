74352f5e6af83644266b26128f19636171fd94d8
brucengdev <brucengdev@gmail.com>
2024-12-31 18:14:51 +0700

    wip

--- src/Backend.Tests/Managers/AccountTests.IsTokenValid.cs
+++ src/Backend.Tests/Managers/AccountTests.IsTokenValid.cs
@@ -7,8 +7,17 @@ namespace Backend.Tests
 {
     public partial class AccountTests
     {
-        [Fact]
-        public void IsTokenValid_must_return_true_if_token_is_valid()
+        public static IEnumerable<object[]> PositiveCases = new List<object[]>
+        {
+            new object[] { new DateTime(2024, 12, 31, 19, 4, 0) },
+            new object[] { new DateTime(2024, 12, 31, 19, 3, 0) },
+            new object[] { new DateTime(2024, 12, 31, 18, 4, 0) },
+            new object[] { new DateTime(2024, 12, 31, 17, 4, 0) },
+        };
+        
+        [Theory]
+        [MemberData(nameof(PositiveCases))]
+        public void IsTokenValid_must_return_true_if_token_is_valid(DateTime currentTime)
         {
             //arrange
             var userRepo = new TestUserRepository();
@@ -20,7 +29,6 @@ namespace Backend.Tests
             var sut = new AccountManager(userRepo);
             
             //act
-            var currentTime = new DateTime(2024, 12, 31, 19, 4, 0);
             var result = sut.IsTokenValid("johndoe-2024-12-31-19-04", currentTime);
             
             //assert
