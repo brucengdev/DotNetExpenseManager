bc40d0a951c88448375310ac719b618fc7ded67a
brucengdev <brucengdev@gmail.com>
2025-01-01 09:40:17 +0700

    wip

--- src/Backend.Tests/Managers/EntryManagerTests.AddEntry.cs
+++ src/Backend.Tests/Managers/EntryManagerTests.AddEntry.cs
@@ -32,5 +32,24 @@ namespace Backend.Tests
             savedEntry.Value.ShouldBe(-10.22f);
             savedEntry.UserId.ShouldBe(23);
         }
+
+        [Theory]
+        [InlineData(0)]
+        public void AddEntry_must_throw_invalid_user_id_on_invalid_user_id(int userId)
+        {
+            //arrange
+            var entryRepo = new TestEntryRepository();
+            var sut = new EntryManager(entryRepo);
+            
+            //act & assert
+            var inputEntry = new Entry()
+            {
+                Title = "Test entry",
+                Value = -10.22f,
+                Date = new DateTime(2022, 4, 22),
+                UserId = userId
+            };
+            Should.Throw<InvalidUserIdException>(() => sut.AddEntry(inputEntry));
+        }
     }
 }
--- src/Backend/Manager/EntryManager.cs
+++ src/Backend/Manager/EntryManager.cs
@@ -3,6 +3,11 @@ using Backend.Repository;
 
 namespace Backend.Manager;
 
+internal class InvalidUserIdException : Exception
+{
+    
+}
+
 internal class EntryManager: IEntryManager
 {
     private readonly IEntryRepository _entryRepository;
@@ -12,6 +17,10 @@ internal class EntryManager: IEntryManager
     }
     public bool AddEntry(Entry input)
     {
+        if (input.UserId == 0)
+        {
+            throw new InvalidUserIdException();
+        }
         return _entryRepository.AddEntry(input);
     }
 }
