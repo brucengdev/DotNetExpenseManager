1dd71b334cb27fbb11a362961f0e8a19fba9c2cf
brucengdev <brucengdev@gmail.com>
2025-01-05 15:15:45 +0700

    wip

--- src/webfrontend/src/storage/Storage.ts
+++ src/webfrontend/src/storage/Storage.ts
@@ -4,3 +4,13 @@ export interface IStorage {
     Set: (key: string, value: any) => void
     Get: (key: string) => any
 }
+
+export class Storage implements IStorage {
+    public Set(key: string, value: any) {
+        localStorage.setItem(key, value)
+    }
+
+    public Get(key: string) {
+        return localStorage.getItem(key)
+    }
+}
