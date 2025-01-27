830d57ca26a52a31308daa5254fdb156e8eecd6e
brucengdev <brucengdev@gmail.com>
2025-01-27 17:00:11 +0700

    wip

--- src/webfrontend/src/__test__/Entry.test.ts
+++ src/webfrontend/src/__test__/Entry.test.ts
@@ -17,4 +17,12 @@ describe("Entry", () => {
         entry2.categoryId = 2
         expect(entry1.Equals(entry2)).toBeFalsy()
     })
+
+    test("Equals must be false when dates are different", () => {
+        const entry1 = new Entry(1, new Date(2024, 12, 1), "foo", -12.22)
+        entry1.categoryId = 1
+        const entry2 = new Entry(1, new Date(2024, 12, 2), "foo", -12.22)
+        entry2.categoryId = 1
+        expect(entry1.Equals(entry2)).toBeFalsy()
+    })
 })
