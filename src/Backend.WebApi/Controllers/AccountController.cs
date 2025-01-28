6b17ecdc425ff190a3b07de09e5f8ac07ec139af
brucengdev <brucengdev@gmail.com>
2025-01-28 17:14:06 +0700

    secure CreateUser

--- src/Backend.WebApi/Controllers/AccountController.cs
+++ src/Backend.WebApi/Controllers/AccountController.cs
@@ -34,6 +34,7 @@ public class AccountController: ControllerBase
     }
 
     [HttpPost("[action]")]
+    [ServiceFilter(typeof(SecurityFilterAttribute))]
     public ActionResult<bool> CreateUser(
         [FromForm] string username, 
         [FromForm] string password)
