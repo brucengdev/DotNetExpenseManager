56650982d739049eb3131ed8169c4ec49c951a07
brucengdev <brucengdev@gmail.com>
2025-01-28 15:43:40 +0700

    wip

--- src/Backend.WebApi.Tests/ActionFilters/SecurityFilterAttributeTests.cs
+++ src/Backend.WebApi.Tests/ActionFilters/SecurityFilterAttributeTests.cs
@@ -4,20 +4,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
+using Shouldly;
 
-namespace Backend.WebApi.Tests.Middleware;
+namespace Backend.WebApi.Tests.ActionFilters;
 
 public class SecurityFilterAttributeTests
 {
     [Fact]
-    public async Task Must_execute_next_middleware_when_valid()
+    public async Task Must_execute_next_stage_when_valid()
     {
         //arrange
         var sut = new SecurityFilterAttribute();
         
         //act
         var actionContext = new ActionContext();
-        actionContext.HttpContext = new DefaultHttpContext();
+        var httpContext = new DefaultHttpContext();
+        actionContext.HttpContext = httpContext;
         actionContext.RouteData = new RouteData();
         actionContext.ActionDescriptor = new ActionDescriptor();
         var context = new ActionExecutingContext(
@@ -25,5 +27,8 @@ public class SecurityFilterAttributeTests
             new List<IFilterMetadata>(),
             new Dictionary<string, object?>(), new object());
         sut.OnActionExecuting(context);
+        
+        //assert
+        context.Result.ShouldBeNull();
     }
 }
