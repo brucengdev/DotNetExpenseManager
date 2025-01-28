ba54194678e092659b6780c897b5ab9eb64b2b35
brucengdev <brucengdev@gmail.com>
2025-01-28 15:14:23 +0700

    wip

--- src/Backend.WebApi.Tests/Middleware/SecurityMiddlewareTests.cs
+++ src/Backend.WebApi.Tests/Middleware/SecurityMiddlewareTests.cs
@@ -1,21 +1,27 @@
 using Backend.WebApi.Middleware;
 using Microsoft.AspNetCore.Http;
+using Shouldly;
 
 namespace Backend.WebApi.Tests.Middleware;
 
 public class SecurityMiddlewareTests
 {
     [Fact]
-    public void Must_set_auth_token_to_http_context()
+    public async Task Must_execute_next_middleware_when_valid()
     {
         //arrange
+        bool movedToNextMiddleware = false;
         var sut = new SecurityMiddleware(async (context) =>
         {
+            movedToNextMiddleware = true;
         });
         
         //act
         var testContext = new DefaultHttpContext();
         testContext.Request.Path = "/test?accessToken=foo";
-        sut.InvokeAsync(testContext);
+        await sut.InvokeAsync(testContext);
+        
+        //assert
+        movedToNextMiddleware.ShouldBeTrue();
     }
 }
