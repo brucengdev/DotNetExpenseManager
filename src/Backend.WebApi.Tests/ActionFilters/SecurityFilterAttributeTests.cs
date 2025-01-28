5059592a112255d7417b486413d6c4ffd5cb9fa3
brucengdev <brucengdev@gmail.com>
2025-01-28 16:03:45 +0700

    wip

--- src/Backend.WebApi.Tests/ActionFilters/SecurityFilterAttributeTests.cs
+++ src/Backend.WebApi.Tests/ActionFilters/SecurityFilterAttributeTests.cs
@@ -1,9 +1,11 @@
+using Backend.Core.Manager;
 using Backend.WebApi.ActionFilters;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
+using Moq;
 using Shouldly;
 
 namespace Backend.WebApi.Tests.ActionFilters;
@@ -14,11 +16,14 @@ public class SecurityFilterAttributeTests
     public async Task Must_execute_next_stage_when_valid()
     {
         //arrange
-        var sut = new SecurityFilterAttribute();
+        var accountManager = new Mock<IAccountManager>();
+        var sut = new SecurityFilterAttribute(accountManager.Object);
         
         //act
         var actionContext = new ActionContext();
         var httpContext = new DefaultHttpContext();
+        var queryString = new QueryString("?accessToken=123");
+        httpContext.Request.QueryString = queryString;
         actionContext.HttpContext = httpContext;
         actionContext.RouteData = new RouteData();
         actionContext.ActionDescriptor = new ActionDescriptor();
@@ -30,5 +35,32 @@ public class SecurityFilterAttributeTests
         
         //assert
         context.Result.ShouldBeNull();
+    }
+    
+    [Fact]
+    public async Task Must_cancel_and_return_401_when_unauthorized()
+    {
+        //arrange
+        var accountManager = new Mock<IAccountManager>();
+        accountManager.Setup(am => am.GetUserId("123", It.IsAny<DateTime>()))
+            .Throws(new UserNotFoundException());
+        var sut = new SecurityFilterAttribute(accountManager.Object);
+        
+        //act
+        var actionContext = new ActionContext();
+        var httpContext = new DefaultHttpContext();
+        var queryString = new QueryString("?accessToken=123");
+        httpContext.Request.QueryString = queryString;
+        actionContext.HttpContext = httpContext;
+        actionContext.RouteData = new RouteData();
+        actionContext.ActionDescriptor = new ActionDescriptor();
+        var context = new ActionExecutingContext(
+            actionContext, 
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object?>(), new object());
+        sut.OnActionExecuting(context);
+        
+        //assert
+        context.Result.ShouldNotBeNull();
     }
 }
--- src/Backend.WebApi/ActionFilters/SecurityFilterAttribute.cs
+++ src/Backend.WebApi/ActionFilters/SecurityFilterAttribute.cs
@@ -1,12 +1,29 @@
+using Backend.Core.Manager;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Backend.WebApi.ActionFilters;
 
 public class SecurityFilterAttribute: IActionFilter
 {
+    private readonly IAccountManager _accountManager;
+    public SecurityFilterAttribute(IAccountManager accountManager)
+    {
+        _accountManager = accountManager;
+    }
+    
     public void OnActionExecuting(ActionExecutingContext context)
     {
-        // Do something before the action executes.
+        var accessToken = context.HttpContext.Request.Query["accessToken"][0];
+        try
+        {
+            var userId = _accountManager.GetUserId(accessToken, DateTime.Now);
+        }
+        catch (UserNotFoundException unfe)
+        {
+            //cancel the pipeline;
+            context.Result = new UnauthorizedResult();
+        }
     }
 
     public void OnActionExecuted(ActionExecutedContext context)
