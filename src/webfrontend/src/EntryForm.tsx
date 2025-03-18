3e2c692f27031b3daf9c58303d0beeef41815bf5
brucengdev <brucengdev@gmail.com>
2025-03-18 08:16:10 +0700

    Make category field in Entry nullable on webfrontend

--- src/webfrontend/src/EntryForm.tsx
+++ src/webfrontend/src/EntryForm.tsx
@@ -18,7 +18,7 @@ export const EntryForm = (props: EntryFormProps) => {
     const [date, setDate] = useState(initialDate)
     const [title, setTitle] = useState("")
     const [value, setValue] = useState("0")
-    const [categoryId, setCategoryId] = useState(0)
+    const [categoryId, setCategoryId] = useState(undefined as number | undefined)
     return <div data-testid="entry-form">
         <label>
             Title
