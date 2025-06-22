a0a2a74b156b432670af05c2f93e95350f5c9384
brucengdev <brucengdev@gmail.com>
2025-06-22 19:02:06 +0700

    wip

--- src/webfrontend/src/MainView.tsx
+++ src/webfrontend/src/MainView.tsx
@@ -9,6 +9,9 @@ export interface MainViewProps {
 
 export function MainView({client, onLogout} : MainViewProps) {
     return <div data-testid="main-view" className="row-auto">
+      <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-2xl font-semibold text-gray-900">Expenses</h2>
+      </div>
       <div className="mb-5">
         <Button text="Day" mode={ButtonMode.PRIMARY}/>
       </div>
