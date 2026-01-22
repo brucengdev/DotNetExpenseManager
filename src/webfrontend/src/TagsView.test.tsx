9a568cf677914c20d5ba3ad10f74f7e8be5b6d18
brucengdev <brucengdev@gmail.com>
2026-01-22 13:04:47 +0700

    Added UI for tag view.

---
+++ src/webfrontend/src/TagsView.test.tsx
@@ -0,0 +1,12 @@
+import { screen, render } from "@testing-library/react";
+import {describe, expect, it} from 'vitest'
+import '@testing-library/jest-dom'
+import { TagsView } from "./TagsView";
+
+describe("TagsView", () => {
+    it("has necessary ui components", () => {
+        render(<TagsView />)
+        expect(screen.queryAllByTestId("tag")).toHaveLength(1)
+        expect(screen.getByRole("button", { name: "+"})).toBeInTheDocument()
+    })
+})
--- src/webfrontend/src/TagsView.tsx
+++ src/webfrontend/src/TagsView.tsx
@@ -1,4 +1,11 @@
-export function TagsView() {
-    return <div data-testid="tags-view">
-        </div>
+import { Button } from "./controls/Button"
+
+export interface TagsViewProps {
+}
+
+export const TagsView = (_: TagsViewProps) => {
+    return <div data-testid="tags-view" className="mb-5">
+        <div data-testid="tag">Sample tag</div>
+        <Button text="+" />
+    </div>
 }
