19fa6624ec23e9040d278bf995e1dbfcc6ffb67c
brucengdev <brucengdev@gmail.com>
2026-03-15 15:14:44 +0700

    Allow to change month.

--- src/webfrontend/src/reports/MonthlyReportView.test.tsx
+++ src/webfrontend/src/reports/MonthlyReportView.test.tsx
@@ -1,11 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { MonthlyReportView } from "./MonthlyReportView";
 import '@testing-library/jest-dom'
+import { TestClient } from "../__test__/TestClient";
 
 describe("MonthlyReportView", () => {
-    it("shows report of current month", () => {
-        render(<MonthlyReportView month={new Date(2026, 2)} />)
+    it("Shows the month picker", () => {
+        const client = new TestClient()
+        render(<MonthlyReportView client={client} month={new Date(2026, 2)} />)
 
         expect(screen.getByTestId("monthly-report-view")).toBeInTheDocument()
 
@@ -13,5 +15,21 @@ describe("MonthlyReportView", () => {
         expect(monthControl).toBeInTheDocument()
 
         expect(monthControl).toHaveValue("2026-03")
+    })
+
+    it("Moves to another month when month picker changes", () => {
+        const client = new TestClient()
+        render(<MonthlyReportView client={client} month={new Date(2026, 2)} />)
+
+        expect(screen.getByTestId("monthly-report-view")).toBeInTheDocument()
+
+        const monthControl = screen.getByLabelText("Month")
+        expect(monthControl).toBeInTheDocument()
+
+        expect(monthControl).toHaveValue("2026-03")
+
+        fireEvent.change(monthControl, { target: { value: "2024-08"} })
+
+        expect(monthControl).toHaveValue("2024-08")
     })
 })
--- src/webfrontend/src/reports/MonthlyReportView.tsx
+++ src/webfrontend/src/reports/MonthlyReportView.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react"
+import { IClient } from "../api/Client"
 import { formatDateToMonthYear } from "../utils"
 
 interface MonthlyReportViewProps {
-    month: Date
+    month: Date,
+    client: IClient
 }
 
 export function MonthlyReportView(props: MonthlyReportViewProps) {
-    const { month } = props
+    const { month: initialMonth } = props
+    const [ month, setMonth ] = useState(initialMonth)
     return <div data-testid="monthly-report-view">
         Monthly Report
         <div>
             <label htmlFor="month-control">Month</label>
-            <input id="month-control" type="month" value={formatDateToMonthYear(month)}/>
+            <input 
+                id="month-control" 
+                type="month" 
+                value={formatDateToMonthYear(month)}
+                onChange={e => setMonth(new Date(e.target.value))}
+            />
         </div>
     </div>
 }
