4ee97f20946c53c31f1608fae0125c4b2da66787
brucengdev <brucengdev@gmail.com>
2026-03-15 18:19:46 +0700

    Update test.

--- src/webfrontend/src/reports/MonthlyReportView.test.tsx
+++ src/webfrontend/src/reports/MonthlyReportView.test.tsx
@@ -3,39 +3,10 @@ import { describe, expect, it, vitest } from "vitest";
 import { MonthlyReportView } from "./MonthlyReportView";
 import '@testing-library/jest-dom'
 import { TestClient } from "../__test__/TestClient";
-import { Entry } from "../models/Entry";
-import { Category } from "../models/Category";
+import { formatDateToMonthYear } from "../utils";
 
 describe("MonthlyReportView", () => {
-    it("Shows the month picker", () => {
-        const client = new TestClient()
-        render(<MonthlyReportView client={client} month={new Date(2026, 2)} />)
-
-        expect(screen.getByTestId("monthly-report-view")).toBeInTheDocument()
-
-        const monthControl = screen.getByLabelText("Month")
-        expect(monthControl).toBeInTheDocument()
-
-        expect(monthControl).toHaveValue("2026-03")
-    })
-
-    it("Moves to another month when month picker changes", () => {
-        const client = new TestClient()
-        render(<MonthlyReportView client={client} month={new Date(2026, 2)} />)
-
-        expect(screen.getByTestId("monthly-report-view")).toBeInTheDocument()
-
-        const monthControl = screen.getByLabelText("Month")
-        expect(monthControl).toBeInTheDocument()
-
-        expect(monthControl).toHaveValue("2026-03")
-
-        fireEvent.change(monthControl, { target: { value: "2024-08"} })
-
-        expect(monthControl).toHaveValue("2024-08")
-    })
-
-    it("Shows report of selected month", async () => {
+     it("Shows month picker and report of selected month", async () => {
         const client = new TestClient()
 
         client.GetMonthlyReport = vitest.fn(async(month: Date) => {
@@ -77,6 +48,73 @@ describe("MonthlyReportView", () => {
                 "Food: -3",
                 "Travel: -3",
                 "Salary: 30"
+            ]
+        )
+    })
+    it("Moves to another month when month picker changes", async () => {
+        const client = new TestClient()
+        client.GetMonthlyReport = vitest.fn(async(month: Date) => {
+            if(formatDateToMonthYear(month) === '2025-03') {
+                return {
+                    byCategories: {
+                        "Household": -2,
+                        "Food": -3,
+                        "Travel": -3,
+                        "Salary": 30
+                    },
+                    totalSpendings: -10,
+                    totalIncome: 30,
+                    savings: 20
+                }
+            }
+
+            //2024-02
+            return {
+                byCategories: {
+                    "Household": -20,
+                    "Food": -30,
+                    "Travel": -30,
+                    "Salary": 300
+                },
+                totalSpendings: -122,
+                totalIncome: 300,
+                savings: 200
+            }
+        })
+        render(<MonthlyReportView client={client} month={new Date(2026, 2)} />)
+
+        expect(screen.getByTestId("monthly-report-view")).toBeInTheDocument()
+
+        const monthControl = screen.getByLabelText("Month")
+        expect(monthControl).toBeInTheDocument()
+
+        expect(monthControl).toHaveValue("2026-03")
+
+        await waitFor(() => {
+            expect(screen.getByTestId("total-spendings")).toHaveTextContent("Total spendings: -10")
+        })
+
+        fireEvent.change(monthControl, {target: {value: "2024-02"}})
+        expect(monthControl).toHaveValue("2024-02")
+
+        await waitFor(() => {
+            expect(screen.getByTestId("total-spendings")).toHaveTextContent("Total spendings: -122")
+        })
+
+        expect(screen.getByTestId("total-income")).toHaveTextContent("Total income: 300")
+        expect(screen.getByTestId("savings")).toHaveTextContent("Savings: 200")
+
+        const byCategories = screen.getByTestId("by-categories")
+        expect(byCategories).toBeInTheDocument()
+
+        const catSummaries = within(byCategories).queryAllByTestId("category-summary")
+        const catSummaryTexts = catSummaries.map(e => e.textContent)
+        expect(catSummaryTexts).toStrictEqual(
+            [
+                "Household: -20",
+                "Food: -30",
+                "Travel: -30",
+                "Salary: 300"
             ]
         )
     })
