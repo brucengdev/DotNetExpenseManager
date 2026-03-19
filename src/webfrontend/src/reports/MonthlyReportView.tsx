74a5f795a4e74b5fff5deafa17161ec8eb69444b
brucengdev <brucengdev@gmail.com>
2026-03-19 12:31:30 +0700

    Update UI

--- src/webfrontend/src/reports/MonthlyReportView.tsx
+++ src/webfrontend/src/reports/MonthlyReportView.tsx
@@ -63,7 +63,8 @@ interface CategorySummaryProps {
 }
 function TableEntry(props: CategorySummaryProps) {
     const { value, dataTestId, label } = props
-    return <div key={label} data-testid={dataTestId}>
-            {label}: {formatMoney(value)}
+    return <div key={label} data-testid={dataTestId} className="grid grid-cols-2">
+            <div className="bg-blue-400 text-white border-1">{label}</div>
+            <div className="border-1">{formatMoney(value)}</div>
         </div>
 }
