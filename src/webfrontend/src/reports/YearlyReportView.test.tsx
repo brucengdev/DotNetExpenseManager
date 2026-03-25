69c8976b6815463fcde173032235f672625b7130
brucengdev <brucengdev@gmail.com>
2026-03-25 22:43:09 +0700

    Make year picker set by default.

--- src/webfrontend/src/reports/YearlyReportView.test.tsx
+++ src/webfrontend/src/reports/YearlyReportView.test.tsx
@@ -5,11 +5,13 @@ import "@testing-library/jest-dom"
 
 describe("Yearrly report", () => 
     it("has UI components", async () => {
-        render(<YearlyReportView />)
+        render(<YearlyReportView year={2023} />)
 
         expect(screen.getByTestId("yearly-report-view")).toBeInTheDocument()
 
         const yearPicker = screen.getByRole("combobox", { name: "Year"})
         expect(yearPicker).toBeInTheDocument()
+
+        expect(yearPicker).toHaveValue("2023")
     })
 )
--- src/webfrontend/src/reports/YearlyReportView.tsx
+++ src/webfrontend/src/reports/YearlyReportView.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { Select } from "../controls/Select";
 
-export function YearlyReportView() {
+interface YearlyReportViewProps {
+    year?: number
+}
+export function YearlyReportView(props: YearlyReportViewProps) {
+    const { year: initialYear } = props
+    const [year, setYear] = useState(initialYear ?? (new Date()).getFullYear())
     return <div data-testid="yearly-report-view">
         <Select
             elementId="year-control"
             label="Year"
-            options={[]}
-            value=""
+            options={[
+                { value: year.toString(), text: year.toString() }
+            ]}
+            value={year.toString()}
         />
     </div>
 }
