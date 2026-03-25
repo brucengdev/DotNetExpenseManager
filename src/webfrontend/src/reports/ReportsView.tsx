821d38d479ebd531a3b20def80740468cf4ce2f4
brucengdev <brucengdev@gmail.com>
2026-03-25 12:37:38 +0700

    Change button style based on current report view.

--- src/webfrontend/src/reports/ReportsView.tsx
+++ src/webfrontend/src/reports/ReportsView.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { IClient } from "../api/Client"
 import { Button, ButtonMode } from "../controls/Button"
 import { MonthlyReportView } from "./MonthlyReportView"
@@ -6,11 +7,21 @@ interface ReportsViewProps {
     client: IClient
 }
 
+enum CurrentReportView {
+    MONTHLY,
+    YEARLY
+}
+
 export function ReportsView(props: ReportsViewProps) {
     const { client } = props
+    const [currentView, setCurrentView] = useState(CurrentReportView.MONTHLY)
     return <div data-testid="reports-view">
-        <Button text="Monthly" mode={ButtonMode.PRIMARY} />
-        <Button text="Yearly" mode={ButtonMode.SECONDARY} />
+        <Button text="Monthly" mode={currentView == CurrentReportView.MONTHLY? ButtonMode.PRIMARY: ButtonMode.SECONDARY} 
+            onClick={() => setCurrentView(CurrentReportView.MONTHLY)}
+        />
+        <Button text="Yearly" mode={currentView == CurrentReportView.YEARLY? ButtonMode.PRIMARY: ButtonMode.SECONDARY}
+            onClick={() => setCurrentView(CurrentReportView.YEARLY)}
+        />
         <MonthlyReportView month={new Date()} client={client} />
     </div>
 }
