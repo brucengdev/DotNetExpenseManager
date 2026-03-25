8adc52fcb885855c8f63369607f26f34d94edb86
brucengdev <brucengdev@gmail.com>
2026-03-25 22:21:39 +0700

    Added test to verify test Id of yearly report view.

--- src/webfrontend/src/reports/YearlyReportView.test.tsx
+++ src/webfrontend/src/reports/YearlyReportView.test.tsx
@@ -1,9 +1,12 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import { YearlyReportView } from "./YearlyReportView";
+import "@testing-library/jest-dom"
 
 describe("Yearrly report", () => 
     it("has UI components", async () => {
         render(<YearlyReportView />)
+
+        expect(screen.getByTestId("yearly-report-view")).toBeInTheDocument()
     })
-})
+)
