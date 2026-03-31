6d09059e4cdab3ddb3585b9af43399db95be8a21
brucengdev <brucengdev@gmail.com>
2026-03-31 12:44:53 +0700

    WIP test for EntryFiltersView.

---
+++ src/webfrontend/src/EntryFiltersView.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { EntryFiltersView } from "./EntryFiltersView";
+import "@testing-library/jest-dom"
+import { TestClient } from "./__test__/TestClient";
+import { Category } from "./models/Category";
+import { Payee } from "./models/Payee";
+import { Tag } from "./models/Tag";
+
+describe("EntryFiltersView", () => {
+    it("has necessary UI", () => {
+        const client = new TestClient()
+        client.Categories = [
+            new Category(1, "household") 
+        ]
+        client.Tags = [
+            new Tag(1, "tag1")
+        ]
+        client.Payees = [
+            new Payee(1, "Tom")
+        ]
+        render(<EntryFiltersView client={client}/>)
+        
+        const fromDateField = screen.getByLabelText("From date")
+        expect(fromDateField).toBeInTheDocument()
+
+        const toDateField = screen.getByLabelText("To date")
+        expect(toDateField).toBeInTheDocument()
+
+        const categoryField = screen.getByTestId("category-control")
+        expect(categoryField).toBeInTheDocument()
+
+        expect(screen.getByLabelText("Tags")).toBeInTheDocument()
+        const tagsField = screen.getByTestId("tags-control")
+        expect(tagsField).toBeInTheDocument()
+        expect(screen.getByRole("option", { name: "tag1"})).toBeInTheDocument()
+
+        expect(screen.getByRole("combobox", { name: "Payee" })).toBeInTheDocument()
+        expect(screen.getByRole("option", { name: "[No payee]"})).toBeInTheDocument()
+        expect((screen.getByRole("option", { name: "[No payee]"}) as HTMLOptionElement).selected).toBeTruthy()
+        expect(screen.getByRole("option", { name: "Tom"})).toBeInTheDocument()
+
+    })
+})
--- src/webfrontend/src/EntryFiltersView.tsx
+++ src/webfrontend/src/EntryFiltersView.tsx
@@ -1,5 +1,12 @@
+import { TextBox } from "./controls/TextBox";
+
 export function EntryFiltersView() {
     return <div data-testId="entry-filters-view">
-        
+        <TextBox
+            label="From date"
+            name="from-date-field"
+            type="date"
+            value=""
+        />
     </div>
 }
