3671d69c21f080d818774e4e5cf54eae71f59e3d
brucengdev <brucengdev@gmail.com>
2026-04-06 12:30:21 +0700

    Added tag control.

--- src/webfrontend/src/EntryFiltersView.tsx
+++ src/webfrontend/src/EntryFiltersView.tsx
@@ -1,17 +1,37 @@
 import { Client } from "./api/Client";
+import { CategoryControl } from "./controls/CategoryControl";
+import { LabeledMultiSelect } from "./controls/LabeledMultiSelect";
 import { TextBox } from "./controls/TextBox";
 
 interface EntryFiltersViewProps {
     client: Client
 }
 
-export function EntryFiltersView(_: EntryFiltersViewProps) {
+export function EntryFiltersView(props: EntryFiltersViewProps) {
+    const { client } = props
     return <div data-testId="entry-filters-view">
         <TextBox
             label="From date"
             name="from-date-field"
             type="date"
             value=""
+        />
+        <TextBox
+            label="To date"
+            name="to-date-field"
+            type="date"
+            value=""
+        />
+        <CategoryControl 
+            client={client}
+            categoryId={undefined}
+            onChange={() => {}}
+            />
+        <LabeledMultiSelect
+            label="Tags"
+            selectDataTestId="tags-control"
+            options={[]}
+            selectedValues={[]}
         />
     </div>
 }
