8154c77fac7707a35a1bf966fd265a16d53e954d
brucengdev <brucengdev@gmail.com>
2026-04-06 12:25:12 +0700

    Added label prop to LabeledMultiSelect

--- src/webfrontend/src/controls/LabeledMultiSelect.tsx
+++ src/webfrontend/src/controls/LabeledMultiSelect.tsx
@@ -5,12 +5,13 @@ interface LabeledMutliSelectProps {
     options: Option[]
     selectedValues: string[]
     selectDataTestId?: string
+    label: string
 }
 
 export function LabeledMultiSelect(props: LabeledMutliSelectProps) {
-    const { options, selectedValues, selectDataTestId, onChange } = props
+    const { label, options, selectedValues, selectDataTestId, onChange } = props
     return <div>
-        <label htmlFor="tags-control" className="block text-sm/6 font-semibold text-gray-900">Tags</label>
+        <label htmlFor="tags-control" className="block text-sm/6 font-semibold text-gray-900">{label}</label>
         <MultiSelect
             selectDataTestId={selectDataTestId}
             options={options}
