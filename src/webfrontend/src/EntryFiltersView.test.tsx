053a6c2b67b274e696577f7994df074b7c6ceaea
brucengdev <brucengdev@gmail.com>
2026-04-06 12:46:33 +0700

    Update tests.

--- src/webfrontend/src/EntryFiltersView.test.tsx
+++ src/webfrontend/src/EntryFiltersView.test.tsx
@@ -19,7 +19,8 @@ describe("EntryFiltersView", () => {
             new Tag(3, "tag3")
         ]
         client.Payees = [
-            new Payee(1, "Tom")
+            new Payee(1, "Tom"),
+            new Payee(2, "Jane")
         ]
         render(<EntryFiltersView client={client}/>)
         
@@ -42,10 +43,14 @@ describe("EntryFiltersView", () => {
             expect(tagNames).toStrictEqual(["tag1","tag2","tag3"])
         })
 
-        expect(screen.getByRole("combobox", { name: "Payee" })).toBeInTheDocument()
-        expect(screen.getByRole("option", { name: "[No payee]"})).toBeInTheDocument()
-        expect((screen.getByRole("option", { name: "[No payee]"}) as HTMLOptionElement).selected).toBeTruthy()
-        expect(screen.getByRole("option", { name: "Tom"})).toBeInTheDocument()
+        const payeeField = screen.getByRole("combobox", { name: "Payee" })
+        expect(payeeField).toBeInTheDocument()
 
+        await waitFor(() => {
+            const tagOptions = within(payeeField).getAllByRole("option")
+            const tagNames = tagOptions.map(to => to.textContent)
+            //payees must be sorted
+            expect(tagNames).toStrictEqual(["[No payee]","Jane","Tom"])
+        })
     })
 })
