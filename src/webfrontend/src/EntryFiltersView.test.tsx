e9b91034b39a984de832363877bd80b07dc9a1bc
brucengdev <brucengdev@gmail.com>
2026-04-07 12:45:06 +0700

    Verify setting of category filters.

--- src/webfrontend/src/EntryFiltersView.test.tsx
+++ src/webfrontend/src/EntryFiltersView.test.tsx
@@ -6,6 +6,8 @@ import { TestClient } from "./__test__/TestClient";
 import { Category } from "./models/Category";
 import { Payee } from "./models/Payee";
 import { Tag } from "./models/Tag";
+import userEvent from "@testing-library/user-event";
+import { CategoryControl } from "./controls/CategoryControl";
 
 describe("EntryFiltersView", () => {
     it("has necessary UI", async () => {
@@ -25,8 +27,8 @@ describe("EntryFiltersView", () => {
             new Payee(1, "Tom"),
             new Payee(2, "Jane")
         ]
-        render(<EntryFiltersView client={client}/>)
-        
+        render(<EntryFiltersView client={client} />)
+
         const fromDateField = screen.getByLabelText("From date")
         expect(fromDateField).toBeInTheDocument()
         expect(fromDateField).toHaveValue("")
@@ -52,7 +54,7 @@ describe("EntryFiltersView", () => {
             const tagOptions = within(tagsField).getAllByRole("option")
             tagOptions.forEach(to => expect(to).not.toBeChecked())
             const tagNames = tagOptions.map(to => to.textContent)
-            expect(tagNames).toStrictEqual(["tag1","tag2","tag3"])
+            expect(tagNames).toStrictEqual(["tag1", "tag2", "tag3"])
         })
 
         const payeeField = screen.getByRole("combobox", { name: "Payee" })
@@ -63,20 +65,41 @@ describe("EntryFiltersView", () => {
             payeeOptions.forEach(po => expect(po).not.toBeChecked())
             const payeeNames = payeeOptions.map(to => to.textContent)
             //payees must be sorted
-            expect(payeeNames).toStrictEqual(["[No payee]","Jane","Tom"])
+            expect(payeeNames).toStrictEqual(["[No payee]", "Jane", "Tom"])
         })
     })
 
     it("updates date filters", () => {
         const client = new TestClient()
-        render(<EntryFiltersView client={client}/>)
+        render(<EntryFiltersView client={client} />)
 
         const fromDateField = screen.getByLabelText("From date")
-        fireEvent.change(fromDateField, { target: { value: "2022-02-22"}})
+        fireEvent.change(fromDateField, { target: { value: "2022-02-22" } })
         expect(fromDateField).toHaveValue("2022-02-22")
 
         const toDateField = screen.getByLabelText("To date")
-        fireEvent.change(toDateField, { target: { value: "2022-02-22"}})
+        fireEvent.change(toDateField, { target: { value: "2022-02-22" } })
         expect(toDateField).toHaveValue("2022-02-22")
+    })
+
+    it("sets value for category filters", async () => {
+        const client = new TestClient()
+        client.Categories = [
+            new Category(1, "Uncategorized"),
+            new Category(2, "household"),
+            new Category(3, "food"),
+            new Category(4, "utilities")
+        ]
+        render(<EntryFiltersView client={client} />)
+
+        const categoryField = screen.getByLabelText("Categories")
+        expect(categoryField).toBeInTheDocument()
+        await waitFor(() => {
+            const categoryOptions = within(categoryField).getAllByRole("option")
+            const categoryNames = categoryOptions.map(co => co.textContent)
+            expect(categoryNames).toStrictEqual(["Uncategorized", "food", "household", "utilities"])
+        })
+        userEvent.selectOptions(categoryField, ["2", "4"])
+        expect(categoryField).toHaveValue(["2","4"])
     })
 })
