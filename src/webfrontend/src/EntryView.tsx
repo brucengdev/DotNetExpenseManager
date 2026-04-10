ad91adafdc546dcc5670cd2b59eb396323dc4ac4
brucengdev <brucengdev@gmail.com>
2026-04-10 12:54:57 +0700

    Shows date in entries.

--- src/webfrontend/src/EntryView.tsx
+++ src/webfrontend/src/EntryView.tsx
@@ -18,8 +18,8 @@ export const EntryView = (props: EntryProps) => {
     const {title, value, categoryName, date,
         tags, payee, notes, onDelete} = props
     const [showConfirmDeletion, setShowConfirmDeletion] = useState(false)
-    let numberOfCols = 6
-    if(date !== undefined) { numberOfCols++ }
+    let numberOfCols = 7
+    if(date !== undefined) { numberOfCols = 8 }
     return <div data-testid="entry" className={`grid grid-cols-${numberOfCols} mb-1`}>
         {
             date?<div data-testid="date">{formatDateToDay(date)}</div>
